887335bd6290c9ce24b2c8eca8a78135452e23d5
ArleyRivasG <arrivasg@unipacifico.edu.co>
2022-11-10 21:30:55 -0500

    Controlar el objeto del mapa con zoomIn() y ZoomOut() + uso de @ViewChild()

--- src/app/mapas/components/zoom-range/zoom-range.component.ts
+++ src/app/mapas/components/zoom-range/zoom-range.component.ts
@@ -1,16 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl'
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
+    `
+    .mapa-container {
+      height: 100%;
+      width: 100%;
+    }
+
+    .row{
+      background-color: white;
+      border-radius: 5px;
+      bottom: 50px;
+      left: 50px;
+      padding: 10px;
+      position: fixed;
+      z-index: 999;
+    }
+    `
   ]
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements AfterViewInit {
 
-  constructor() { }
+  //viewChild me sirve para tomar un elemento HTML y utilizarlo como una propiedad comun y corriente en el ts
+  @ViewChild('mapa') divMapa!: ElementRef;
+  mapa!: mapboxgl.Map;
 
-  ngOnInit(): void {
+  constructor() { 
+  }
+  //AfterViewInit lo usamos para aprovechar que ya el componente se creo  y poder tomar la referecia  con el ViewChild
+  //ya que este en el onInit y en el contructor seria undefined
+  ngAfterViewInit(): void {
+    console.log('afterViewInit', this.divMapa);
+
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [ -76.99872968556471, 3.87874066424086 ],
+      zoom: 18
+    });
+  }
+
+  zoomOut(){
+    this.mapa.zoomOut();
+  }
+
+  zoomIn(){
+    this.mapa.zoomIn();
+
   }
 
 }
