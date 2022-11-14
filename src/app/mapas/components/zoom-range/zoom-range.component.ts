91536f4c69014ceba5717a892767eda69b77934e
ArleyRivasG <arrivasg@unipacifico.edu.co>
2022-11-14 18:41:06 -0500

    Obtener las coordenadas centrales del mapa

--- src/app/mapas/components/zoom-range/zoom-range.component.ts
+++ src/app/mapas/components/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl'
 @Component({
   selector: 'app-zoom-range',
@@ -23,15 +23,23 @@ import * as mapboxgl from 'mapbox-gl'
     `
   ]
 })
-export class ZoomRangeComponent implements AfterViewInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
 
   //viewChild me sirve para tomar un elemento HTML y utilizarlo como una propiedad comun y corriente en el ts
   @ViewChild('mapa') divMapa!: ElementRef;
   mapa!: mapboxgl.Map;
 
   zoomLevel: number = 10;
+  center: [number, number] = [ -76.99872968556471, 3.87874066424086 ];
 
   constructor() { 
+  }
+
+  ngOnDestroy(): void {
+    //Destruimos los on-Listener
+    this.mapa.off('zoom', () => {});
+    this.mapa.off('zoomend', () => {});
+    this.mapa.off('move', () => {});
   }
   //AfterViewInit lo usamos para aprovechar que ya el componente se creo  y poder tomar la referecia  con el ViewChild
   //ya que este en el onInit y en el contructor seria undefined
@@ -40,20 +48,29 @@ export class ZoomRangeComponent implements AfterViewInit {
     this.mapa = new mapboxgl.Map({
       container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [ -76.99872968556471, 3.87874066424086 ],
+      center: this.center,
       zoom: this.zoomLevel
     });
 
     //on() es un listener de mapbox
     this.mapa.on('zoom', (event) => {
       this.zoomLevel = this.mapa.getZoom();
-    })
+    });
 
     this.mapa.on('zoomend' , (ev) => {
       if ( this.mapa.getZoom() > 18 ) {
         this.mapa.zoomTo( 18 );
       }
-    })
+    });
+
+    
+    //Movimiento del mapa
+    this.mapa.on( 'move', (event) =>{
+      const target = event.target;
+      const { lng, lat } = target.getCenter();
+      this.center = [ lng, lat]
+    });
+
   }
 
   zoomOut(){
