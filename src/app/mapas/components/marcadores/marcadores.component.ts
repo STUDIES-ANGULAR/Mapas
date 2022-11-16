a5f76aa5c80e22e7c28befafd50833b42e7e7fbb
ArleyRivasG <arrivasg@unipacifico.edu.co>
2022-11-15 21:04:45 -0500

    Marcadores en el mapa

--- src/app/mapas/components/marcadores/marcadores.component.ts
+++ src/app/mapas/components/marcadores/marcadores.component.ts
@@ -1,16 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
   styles: [
+    ` 
+      .mapa-container {
+        height: 100%;
+        width: 100%;
+      }
+    `
   ]
 })
-export class MarcadoresComponent implements OnInit {
+export class MarcadoresComponent implements AfterViewInit {
+
+  @ViewChild('mapa') divMapa!: ElementRef;
+  mapa!: mapboxgl.Map;
+
+  zoomLevel: number = 15;
+  center: [number, number] = [ -76.99872968556471, 3.87874066424086 ];
+
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel
+    });
+
+    //agregar elemento personalizado al marcador
+    // const markerHTML: HTMLElement = document.createElement('div');
+    // markerHTML.innerHTML = 'Hola Mundo';
+
+    // const marker = new mapboxgl.Marker({
+    //   element: markerHTML
+    // })
+    //         .setLngLat( this.center )
+    //         .addTo( this.mapa );
+
+
+    const marker = new mapboxgl.Marker()
+            .setLngLat( this.center )
+            .addTo( this.mapa );
+
   }
 
+
+
+  
 }
